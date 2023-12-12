b15ebdb0d8cc2b007e50d44de9ccf56b8dd812c0
Sergey Ratiashvili <serg.s.r@gmail.com>
2023-12-12 20:16:37 +0100

    chore: update gaia version

--- integration_tests/dockerfiles/gaia/build.sh
+++ integration_tests/dockerfiles/gaia/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$(dirname $0)"
 cd $DIR
-git clone https://github.com/cosmos/gaia.git -b v14.0.0-rc1
+git clone https://github.com/cosmos/gaia.git -b v14.1.0
 cp ./Dockerfile ./gaia
 docker build gaia -t gaia
 rm -rf ./gaia
