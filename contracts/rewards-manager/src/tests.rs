e259fb3443dafbab2ca8cc5f8de3c87d011f7902
Albert Andrejev <albert.andrejev@gmail.com>
2024-01-18 11:26:12 +0200

    test handler contract call

--- contracts/rewards-manager/src/tests.rs
+++ contracts/rewards-manager/src/tests.rs
@@ -2,15 +2,18 @@ use crate::contract::instantiate;
 
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
 use cosmwasm_std::{
-    to_json_binary, Addr, Attribute, Coin, Empty, Event, Response, StdError, Uint128,
+    attr, coins, to_json_binary, Addr, Attribute, Coin, Empty, Event, Response, StdError, Uint128,
 };
 use cw_multi_test::{custom_app, App, Contract, ContractWrapper, Executor};
+use lido_helpers::answer::response;
 use lido_staking_base::msg::rewards_manager::QueryMsg;
 use lido_staking_base::msg::rewards_manager::{ExecuteMsg, InstantiateMsg};
 use lido_staking_base::state::rewards_manager::HandlerConfig;
 
 const CORE_CONTRACT_ADDR: &str = "core_contract";
 const HANDLER_CONTRACT_ADDR: &str = "handler_contract";
+
+const SENDER_ADDR: &str = "sender";
 
 fn instantiate_contract(
     app: &mut App,
@@ -32,7 +35,14 @@ fn instantiate_contract(
 fn handler_contract() -> Box<dyn Contract<Empty>> {
     let contract: ContractWrapper<Empty, Empty, Empty, StdError, StdError, StdError> =
         ContractWrapper::new(
-            |_, _, _, _: Empty| Ok(Response::new()),
+            |_, _, _, _: Empty| {
+                println!("handler contract execute");
+                Ok(response(
+                    "handler_contract_execute",
+                    "handler_contract",
+                    [attr("processing_denom", "test".to_string())],
+                ))
+            },
             |_, _, _, _: Empty| Ok(Response::new()),
             |_, _, _: Empty| to_json_binary(&{}),
         );
@@ -55,7 +65,7 @@ fn rewards_manager_contract() -> Box<dyn Contract<Empty>> {
 fn instantiate_rewards_manager_contract(app: &mut App, id: u64, msg: InstantiateMsg) -> Addr {
     app.instantiate_contract(
         id,
-        Addr::unchecked(CORE_CONTRACT_ADDR),
+        Addr::unchecked("sender"),
         &msg,
         &[],
         "rewards manager contract",
@@ -65,7 +75,18 @@ fn instantiate_rewards_manager_contract(app: &mut App, id: u64, msg: Instantiate
 }
 
 fn mock_app() -> App {
-    custom_app(|_r, _a, _s| {})
+    custom_app(|r, _a, s| {
+        r.bank
+            .init_balance(
+                s,
+                &Addr::unchecked(SENDER_ADDR),
+                vec![Coin {
+                    denom: "ueth".to_string(),
+                    amount: Uint128::from(1000000000u128),
+                }],
+            )
+            .unwrap();
+    })
 }
 
 #[test]
@@ -119,8 +140,9 @@ fn test_config_query() {
 }
 
 #[test]
-fn test_add_handler() {
+fn test_add_remove_handler() {
     let mut app = mock_app();
+
     let handler_contract = instantiate_handler_contract(&mut app);
 
     let rewards_manager_code_id = app.store_code(rewards_manager_contract());
@@ -139,7 +161,6 @@ fn test_add_handler() {
         min_rewards: Uint128::zero(),
     };
 
-    let funds: Vec<Coin> = Vec::new();
     let res = app
         .execute_contract(
             Addr::unchecked(CORE_CONTRACT_ADDR),
@@ -147,21 +168,74 @@ fn test_add_handler() {
             &ExecuteMsg::AddHandler {
                 config: handler_config.clone(),
             },
-            &funds,
+            &[],
         )
         .unwrap();
 
+    let ty = res.events[1].ty.clone();
+
     assert_eq!(
-        res.events[1],
-        Event::new("crates.io:lido-staking__lido-rewards-manager-add_handler".to_string())
-            .add_attributes(vec![
-                Attribute::new("denom".to_string(), handler_config.denom.to_string()),
-                Attribute::new("address".to_string(), handler_config.address.to_string()),
-                Attribute::new(
-                    "min_rewards".to_string(),
-                    handler_config.min_rewards.to_string()
-                ),
-            ])
+        ty,
+        "wasm-crates.io:lido-staking__lido-rewards-manager-add_handler".to_string()
+    );
+
+    let attrs = res.events[1].attributes[1..].to_vec();
+
+    assert_eq!(
+        attrs,
+        vec![
+            Attribute::new("denom".to_string(), handler_config.denom.to_string()),
+            Attribute::new("address".to_string(), handler_config.address.to_string()),
+            Attribute::new(
+                "min_rewards".to_string(),
+                handler_config.min_rewards.to_string()
+            ),
+        ]
+    );
+
+    let config: lido_staking_base::msg::rewards_manager::ConfigResponse = app
+        .wrap()
+        .query_wasm_smart(rewards_manager_contract.clone(), &QueryMsg::Config {})
+        .unwrap();
+
+    assert_eq!(
+        config,
+        lido_staking_base::msg::rewards_manager::ConfigResponse {
+            core_address: CORE_CONTRACT_ADDR.to_string(),
+            handlers: vec![HandlerConfig {
+                address: handler_config.address.clone(),
+                denom: handler_config.denom.clone(),
+                min_rewards: Uint128::zero()
+            }]
+        }
+    );
+
+    let res = app
+        .execute_contract(
+            Addr::unchecked(CORE_CONTRACT_ADDR),
+            rewards_manager_contract.clone(),
+            &ExecuteMsg::RemoveHandler {
+                denom: handler_config.denom.clone(),
+            },
+            &[],
+        )
+        .unwrap();
+
+    let ty = res.events[1].ty.clone();
+
+    assert_eq!(
+        ty,
+        "wasm-crates.io:lido-staking__lido-rewards-manager-remove_handler".to_string()
+    );
+
+    let attrs = res.events[1].attributes[1..].to_vec();
+
+    assert_eq!(
+        attrs,
+        vec![Attribute::new(
+            "denom".to_string(),
+            handler_config.denom.to_string()
+        ),]
     );
 
     let config: lido_staking_base::msg::rewards_manager::ConfigResponse = app
@@ -176,4 +250,56 @@ fn test_add_handler() {
             handlers: vec![]
         }
     );
+}
+
+#[test]
+fn test_handler_call() {
+    let mut app = mock_app();
+
+    let sender_address = Addr::unchecked(SENDER_ADDR);
+
+    let handler_contract = instantiate_handler_contract(&mut app);
+
+    let rewards_manager_code_id = app.store_code(rewards_manager_contract());
+
+    let rewards_manager_contract = instantiate_rewards_manager_contract(
+        &mut app,
+        rewards_manager_code_id,
+        InstantiateMsg {
+            core_address: CORE_CONTRACT_ADDR.to_string(),
+        },
+    );
+
+    let amount = coins(100, "ueth");
+    let _ = app
+        .send_tokens(sender_address, rewards_manager_contract.clone(), &amount)
+        .unwrap();
+
+    let handler_config = HandlerConfig {
+        address: handler_contract.to_string(),
+        denom: "ueth".to_string(),
+        min_rewards: Uint128::zero(),
+    };
+
+    let _res = app
+        .execute_contract(
+            Addr::unchecked(CORE_CONTRACT_ADDR),
+            rewards_manager_contract.clone(),
+            &ExecuteMsg::AddHandler {
+                config: handler_config.clone(),
+            },
+            &[],
+        )
+        .unwrap();
+
+    let res = app
+        .execute_contract(
+            Addr::unchecked(CORE_CONTRACT_ADDR),
+            rewards_manager_contract.clone(),
+            &ExecuteMsg::ExchangeRewards {},
+            &[],
+        )
+        .unwrap();
+
+    println!("res: {:?}", res);
 }
