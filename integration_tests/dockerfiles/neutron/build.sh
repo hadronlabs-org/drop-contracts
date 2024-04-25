ae22e4d46f4bd54aa45f02c60f6323b086ce90de
Sergey Ratiashvili <serg.s.r@gmail.com>
2024-04-25 21:48:36 +0200

    feat: fml

--- integration_tests/dockerfiles/neutron/build.sh
+++ integration_tests/dockerfiles/neutron/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$(dirname $0)"
-COMMIT_HASH_OR_BRANCH="main"
+COMMIT_HASH_OR_BRANCH="feat/slinky"
 cd $DIR
 VERSION=$(cat ../../package.json | jq -r '.version')
 if [[ "$CI" == "true" ]]; then
