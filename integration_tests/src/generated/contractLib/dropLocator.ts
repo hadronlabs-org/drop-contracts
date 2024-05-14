eeb81f155890a770ee0206f17c6ef1ebf7e59b36
Victor Faust <vl.averkov@mail.ru>
2024-05-14 13:18:14 +0200

    Move schema generator, add dropLocator to contractLib

---
+++ integration_tests/src/generated/contractLib/dropLocator.ts
@@ -0,0 +1,195 @@
+import { CosmWasmClient, SigningCosmWasmClient, ExecuteResult, InstantiateResult } from "@cosmjs/cosmwasm-stargate"; 
+import { StdFee } from "@cosmjs/amino";
+import { Coin } from "@cosmjs/amino";
+export type ArrayOfDropInstance = DropInstance1[];
+export type ArrayOfFactoryInstance = FactoryInstance1[];
+/**
+ * Expiration represents a point in time when some event happens. It can compare with a BlockInfo and will return is_expired() == true once the condition is hit (and for every block in the future)
+ */
+export type Expiration =
+  | {
+      at_height: number;
+    }
+  | {
+      at_time: Timestamp;
+    }
+  | {
+      never: {};
+    };
+/**
+ * A point in time in nanosecond precision.
+ *
+ * This type can represent times from 1970-01-01T00:00:00Z to 2554-07-21T23:34:33Z.
+ *
+ * ## Examples
+ *
+ * ``` # use cosmwasm_std::Timestamp; let ts = Timestamp::from_nanos(1_000_000_202); assert_eq!(ts.nanos(), 1_000_000_202); assert_eq!(ts.seconds(), 1); assert_eq!(ts.subsec_nanos(), 202);
+ *
+ * let ts = ts.plus_seconds(2); assert_eq!(ts.nanos(), 3_000_000_202); assert_eq!(ts.seconds(), 3); assert_eq!(ts.subsec_nanos(), 202); ```
+ */
+export type Timestamp = Uint64;
+/**
+ * A thin wrapper around u64 that is using strings for JSON encoding/decoding, such that the full u64 range can be used for clients that convert JSON numbers to floats, like JavaScript and jq.
+ *
+ * # Examples
+ *
+ * Use `from` to create instances of this and `u64` to get the value out:
+ *
+ * ``` # use cosmwasm_std::Uint64; let a = Uint64::from(42u64); assert_eq!(a.u64(), 42);
+ *
+ * let b = Uint64::from(70u32); assert_eq!(b.u64(), 70); ```
+ */
+export type Uint64 = string;
+/**
+ * Actions that can be taken to alter the contract's ownership
+ */
+export type UpdateOwnershipArgs =
+  | {
+      transfer_ownership: {
+        expiry?: Expiration | null;
+        new_owner: string;
+      };
+    }
+  | "accept_ownership"
+  | "renounce_ownership";
+
+export interface DropLocatorSchema {
+  responses: DropInstance | ArrayOfDropInstance | FactoryInstance | ArrayOfFactoryInstance | OwnershipForString;
+  query: FactoryInstanceArgs | ChainArgs;
+  execute: AddChainsArgs | RemoveChainsArgs | UpdateOwnershipArgs;
+  instantiate?: InstantiateMsg;
+  [k: string]: unknown;
+}
+export interface DropInstance {
+  factory_addr: string;
+  name: string;
+}
+export interface DropInstance1 {
+  factory_addr: string;
+  name: string;
+}
+export interface FactoryInstance {
+  addr: string;
+  contracts: State;
+}
+export interface State {
+  core_contract: string;
+  distribution_contract: string;
+  puppeteer_contract: string;
+  rewards_manager_contract: string;
+  staker_contract: string;
+  strategy_contract: string;
+  token_contract: string;
+  validators_set_contract: string;
+  withdrawal_manager_contract: string;
+  withdrawal_voucher_contract: string;
+}
+export interface FactoryInstance1 {
+  addr: string;
+  contracts: State;
+}
+/**
+ * The contract's ownership info
+ */
+export interface OwnershipForString {
+  /**
+   * The contract's current owner. `None` if the ownership has been renounced.
+   */
+  owner?: string | null;
+  /**
+   * The deadline for the pending owner to accept the ownership. `None` if there isn't a pending ownership transfer, or if a transfer exists and it doesn't have a deadline.
+   */
+  pending_expiry?: Expiration | null;
+  /**
+   * The account who has been proposed to take over the ownership. `None` if there isn't a pending ownership transfer.
+   */
+  pending_owner?: string | null;
+}
+export interface FactoryInstanceArgs {
+  name: string;
+}
+export interface ChainArgs {
+  name: string;
+}
+export interface AddChainsArgs {
+  chains: DropInstance1[];
+}
+export interface RemoveChainsArgs {
+  names: string[];
+}
+export interface InstantiateMsg {}
+
+
+function isSigningCosmWasmClient(
+  client: CosmWasmClient | SigningCosmWasmClient
+): client is SigningCosmWasmClient {
+  return 'execute' in client;
+}
+
+export class Client {
+  private readonly client: CosmWasmClient | SigningCosmWasmClient;
+  contractAddress: string;
+  constructor(client: CosmWasmClient | SigningCosmWasmClient, contractAddress: string) {
+    this.client = client;
+    this.contractAddress = contractAddress;
+  }
+  mustBeSigningClient() {
+    return new Error("This client is not a SigningCosmWasmClient");
+  }
+  static async instantiate(
+    client: SigningCosmWasmClient,
+    sender: string,
+    codeId: number,
+    initMsg: InstantiateMsg,
+    label: string,
+    fees: StdFee | 'auto' | number,
+    initCoins?: readonly Coin[],
+  ): Promise<InstantiateResult> {
+    const res = await client.instantiate(sender, codeId, initMsg, label, fees, {
+      ...(initCoins && initCoins.length && { funds: initCoins }),
+    });
+    return res;
+  }
+  static async instantiate2(
+    client: SigningCosmWasmClient,
+    sender: string,
+    codeId: number,
+    salt: number,
+    initMsg: InstantiateMsg,
+    label: string,
+    fees: StdFee | 'auto' | number,
+    initCoins?: readonly Coin[],
+  ): Promise<InstantiateResult> {
+    const res = await client.instantiate2(sender, codeId, new Uint8Array([salt]), initMsg, label, fees, {
+      ...(initCoins && initCoins.length && { funds: initCoins }),
+    });
+    return res;
+  }
+  queryFactoryInstance = async(args: FactoryInstanceArgs): Promise<FactoryInstance> => {
+    return this.client.queryContractSmart(this.contractAddress, { factory_instance: args });
+  }
+  queryFactoryInstances = async(): Promise<ArrayOfFactoryInstance> => {
+    return this.client.queryContractSmart(this.contractAddress, { factory_instances: {} });
+  }
+  queryChain = async(args: ChainArgs): Promise<DropInstance> => {
+    return this.client.queryContractSmart(this.contractAddress, { chain: args });
+  }
+  queryChains = async(): Promise<ArrayOfDropInstance> => {
+    return this.client.queryContractSmart(this.contractAddress, { chains: {} });
+  }
+  queryOwnership = async(): Promise<OwnershipForString> => {
+    return this.client.queryContractSmart(this.contractAddress, { ownership: {} });
+  }
+  addChains = async(sender:string, args: AddChainsArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]): Promise<ExecuteResult> =>  {
+          if (!isSigningCosmWasmClient(this.client)) { throw this.mustBeSigningClient(); }
+    return this.client.execute(sender, this.contractAddress, { add_chains: args }, fee || "auto", memo, funds);
+  }
+  removeChains = async(sender:string, args: RemoveChainsArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]): Promise<ExecuteResult> =>  {
+          if (!isSigningCosmWasmClient(this.client)) { throw this.mustBeSigningClient(); }
+    return this.client.execute(sender, this.contractAddress, { remove_chains: args }, fee || "auto", memo, funds);
+  }
+  updateOwnership = async(sender:string, args: UpdateOwnershipArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]): Promise<ExecuteResult> =>  {
+          if (!isSigningCosmWasmClient(this.client)) { throw this.mustBeSigningClient(); }
+    return this.client.execute(sender, this.contractAddress, { update_ownership: args }, fee || "auto", memo, funds);
+  }
+}
--- integration_tests/src/generated/contractLib/index.ts
+++ integration_tests/src/generated/contractLib/index.ts
@@ -16,41 +16,44 @@ export const DropFactory = _4;
 import * as _5 from './dropHookTester';
 export const DropHookTester = _5;
 
-import * as _6 from './dropPriceProvider';
-export const DropPriceProvider = _6;
+import * as _6 from './dropLocator';
+export const DropLocator = _6;
 
-import * as _7 from './dropProposalVotesPoc';
-export const DropProposalVotesPoc = _7;
+import * as _7 from './dropPriceProvider';
+export const DropPriceProvider = _7;
 
-import * as _8 from './dropProviderProposalsPoc';
-export const DropProviderProposalsPoc = _8;
+import * as _8 from './dropProposalVotesPoc';
+export const DropProposalVotesPoc = _8;
 
-import * as _9 from './dropPump';
-export const DropPump = _9;
+import * as _9 from './dropProviderProposalsPoc';
+export const DropProviderProposalsPoc = _9;
 
-import * as _10 from './dropPuppeteer';
-export const DropPuppeteer = _10;
+import * as _10 from './dropPump';
+export const DropPump = _10;
 
-import * as _11 from './dropRewardsManager';
-export const DropRewardsManager = _11;
+import * as _11 from './dropPuppeteer';
+export const DropPuppeteer = _11;
 
-import * as _12 from './dropStaker';
-export const DropStaker = _12;
+import * as _12 from './dropRewardsManager';
+export const DropRewardsManager = _12;
 
-import * as _13 from './dropStrategy';
-export const DropStrategy = _13;
+import * as _13 from './dropStaker';
+export const DropStaker = _13;
 
-import * as _14 from './dropToken';
-export const DropToken = _14;
+import * as _14 from './dropStrategy';
+export const DropStrategy = _14;
 
-import * as _15 from './dropValidatorsSet';
-export const DropValidatorsSet = _15;
+import * as _15 from './dropToken';
+export const DropToken = _15;
 
-import * as _16 from './dropValidatorsStats';
-export const DropValidatorsStats = _16;
+import * as _16 from './dropValidatorsSet';
+export const DropValidatorsSet = _16;
 
-import * as _17 from './dropWithdrawalManager';
-export const DropWithdrawalManager = _17;
+import * as _17 from './dropValidatorsStats';
+export const DropValidatorsStats = _17;
 
-import * as _18 from './dropWithdrawalVoucher';
-export const DropWithdrawalVoucher = _18;
+import * as _18 from './dropWithdrawalManager';
+export const DropWithdrawalManager = _18;
+
+import * as _19 from './dropWithdrawalVoucher';
+export const DropWithdrawalVoucher = _19;
