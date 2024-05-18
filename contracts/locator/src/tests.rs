81bceb6ba22179dccf60fa19567dcb97eca50531
Victor Faust <81014877+faust403@users.noreply.github.com>
2024-05-18 16:38:11 +0200

    Improve add_factory_instances test

--- contracts/locator/src/tests.rs
+++ contracts/locator/src/tests.rs
@@ -30,39 +30,69 @@ fn add_factory_instances() {
     let deps_mut = deps.as_mut();
     cw_ownable::initialize_owner(deps_mut.storage, deps_mut.api, Some("admin")).unwrap(); // to update admin for contract
 
-    let factory_state = FactoryState {
-        token_contract: String::from("token_contract"),
-        core_contract: String::from("core_contract"),
-        puppeteer_contract: String::from("puppeteer_contract"),
-        staker_contract: String::from("staker_contract"),
-        withdrawal_voucher_contract: String::from("withdrawal_voucher_contract"),
-        withdrawal_manager_contract: String::from("withdrawal_manager_contract"),
-        strategy_contract: String::from("strategy_contract"),
-        validators_set_contract: String::from("validators_set_contract"),
-        distribution_contract: String::from("distribution_contract"),
-        rewards_manager_contract: String::from("rewards_manager_contract"),
-    };
+    let expected_factory_states = vec![
+        FactoryState {
+            token_contract: String::from("token_contract1"),
+            core_contract: String::from("core_contract1"),
+            puppeteer_contract: String::from("puppeteer_contract1"),
+            staker_contract: String::from("staker_contract1"),
+            withdrawal_voucher_contract: String::from("withdrawal_voucher_contract1"),
+            withdrawal_manager_contract: String::from("withdrawal_manager_contract1"),
+            strategy_contract: String::from("strategy_contract1"),
+            validators_set_contract: String::from("validators_set_contract1"),
+            distribution_contract: String::from("distribution_contract1"),
+            rewards_manager_contract: String::from("rewards_manager_contract1"),
+        },
+        FactoryState {
+            token_contract: String::from("token_contract2"),
+            core_contract: String::from("core_contract2"),
+            puppeteer_contract: String::from("puppeteer_contract2"),
+            staker_contract: String::from("staker_contract2"),
+            withdrawal_voucher_contract: String::from("withdrawal_voucher_contract2"),
+            withdrawal_manager_contract: String::from("withdrawal_manager_contract2"),
+            strategy_contract: String::from("strategy_contract2"),
+            validators_set_contract: String::from("validators_set_contract2"),
+            distribution_contract: String::from("distribution_contract2"),
+            rewards_manager_contract: String::from("rewards_manager_contract2"),
+        },
+    ];
 
-    let expected_factory_state = factory_state.clone();
+    let factory_state_1 = expected_factory_states[0].clone();
+    let factory_state_2 = expected_factory_states[1].clone();
 
     // When we call factory (addr) contract we're expecting to get invalid data as the part of expected behaviour
-    deps.querier.add_wasm_query_response("factory", move |msg| {
-        let q: FactoryQueryMsg = from_json(msg).unwrap();
-        match q {
-            FactoryQueryMsg::State {} => to_json_binary(&factory_state).unwrap(),
-            _ => unimplemented!(),
-        }
-    });
+    deps.querier
+        .add_wasm_query_response("factory1", move |msg| {
+            let q: FactoryQueryMsg = from_json(msg).unwrap();
+            match q {
+                FactoryQueryMsg::State {} => to_json_binary(&factory_state_1).unwrap(),
+                _ => unimplemented!(),
+            }
+        });
+    deps.querier
+        .add_wasm_query_response("factory2", move |msg| {
+            let q: FactoryQueryMsg = from_json(msg).unwrap();
+            match q {
+                FactoryQueryMsg::State {} => to_json_binary(&factory_state_2).unwrap(),
+                _ => unimplemented!(),
+            }
+        });
 
     // Drop instance that we'll add and expecting to exist
-    let expected_drop_instance = DropInstance {
-        name: String::from("chain1"),
-        factory_addr: String::from("factory"),
-    };
+    let expected_drop_instances = vec![
+        DropInstance {
+            name: String::from("chain1"),
+            factory_addr: String::from("factory1"),
+        },
+        DropInstance {
+            name: String::from("chain2"),
+            factory_addr: String::from("factory2"),
+        },
+    ];
 
     // Add chain1 with factory as addr of factory instance
     let msg = crate::msg::ExecuteMsg::AddChains {
-        chains: vec![expected_drop_instance.clone()],
+        chains: expected_drop_instances.clone(),
     };
     crate::contract::execute(
         deps.as_mut(),
@@ -72,43 +102,50 @@ fn add_factory_instances() {
     )
     .unwrap();
 
-    // Get chain1 info (iow trigger proxy query)
-    let factory_instance_info1: FactoryInstance = from_json(
-        crate::contract::query(
-            deps.as_ref(),
-            mock_env(),
-            QueryMsg::FactoryInstance {
-                name: String::from("chain1"),
-            },
+    // Query each drop_instance we have and compare it with expacted
+    for (i, edi) in expected_drop_instances.iter().enumerate() {
+        let factory_instance_info: FactoryInstance = from_json(
+            crate::contract::query(
+                deps.as_ref(),
+                mock_env(),
+                QueryMsg::FactoryInstance {
+                    name: edi.name.clone(),
+                },
+            )
+            .unwrap(),
         )
-        .unwrap(),
-    )
-    .unwrap();
-    assert_eq!(
-        factory_instance_info1,
-        FactoryInstance {
-            addr: String::from("factory"),
-            contracts: expected_factory_state
-        }
-    );
-
-    // Get chain1 info (iow trigger STATE)
-    let value_load = STATE
-        .load(deps.as_ref().storage, "chain1".to_string())
         .unwrap();
-    assert_eq!(value_load, expected_drop_instance);
 
-    // Get chain1 info (iow trigger constract's queries)
-    let value_query: DropInstance = from_json(
-        contract::query(
-            deps.as_ref(),
-            mock_env(),
-            QueryMsg::Chain {
-                name: String::from("chain1"),
-            },
+        assert_eq!(
+            factory_instance_info,
+            FactoryInstance {
+                addr: edi.factory_addr.clone(),
+                contracts: expected_factory_states[i].clone()
+            }
+        );
+    }
+
+    // Get all possible chains from contract directly from STATE
+    for drop_instance in expected_drop_instances.clone() {
+        let value_load = STATE
+            .load(deps.as_ref().storage, drop_instance.name.to_string())
+            .unwrap();
+        assert_eq!(value_load, drop_instance);
+    }
+
+    // Get all possible chains from contract from query
+    for drop_instance in expected_drop_instances {
+        let value_query: DropInstance = from_json(
+            contract::query(
+                deps.as_ref(),
+                mock_env(),
+                QueryMsg::Chain {
+                    name: drop_instance.name.to_string(),
+                },
+            )
+            .unwrap(),
         )
-        .unwrap(),
-    )
-    .unwrap();
-    assert_eq!(value_query, expected_drop_instance);
+        .unwrap();
+        assert_eq!(value_query, drop_instance);
+    }
 }
