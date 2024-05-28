886b68a4a0bb7c83955e1c0afc88ea2fa3f92dbc
Albert Andrejev <albert.andrejev@gmail.com>
2024-05-28 16:17:45 +0300

    fix tests

--- integration_tests/src/testSuite.ts
+++ integration_tests/src/testSuite.ts
@@ -183,8 +183,11 @@ type Keys = (typeof keys)[number];
 const awaitFirstBlock = (rpc: string): Promise<void> =>
   waitFor(async () => {
     try {
+      console.log(`Connecting to ${rpc}`);
       const client = await StargateClient.connect(rpc);
+      console.log(`Connected to ${rpc}`);
       const block = await client.getBlock();
+      console.log(`Got block ${block.header.height}`);
       if (block.header.height > 1) {
         return true;
       }
@@ -363,7 +366,7 @@ export const setupPark = async (
   const instance = await cosmopark.create(config);
   await Promise.all(
     Object.entries(instance.ports).map(([network, ports]) =>
-      awaitFirstBlock(`127.0.0.1:${ports.rpc}`).catch((e) => {
+      awaitFirstBlock(`http://127.0.0.1:${ports.rpc}`).catch((e) => {
         console.log(`Failed to await first block for ${network}: ${e}`);
         throw e;
       }),
