54439e851b69b2dc8a9fa4bd70b2958a554045a3
Victor Faust <81014877+faust403@users.noreply.github.com>
2024-06-06 22:55:51 +0200

    Small refactoring

--- scripts/unbonding_batches_visualisation/src/app.ts
+++ scripts/unbonding_batches_visualisation/src/app.ts
@@ -3,17 +3,9 @@ import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { GasPrice } from "@cosmjs/stargate";
 import {
   Client as DropCoreClient,
+  Config as DropCoreConfig,
   UnbondBatch,
 } from "../../../integration_tests/src/generated/contractLib/dropCore";
-
-/* There're 2 possible modes for script
- * RECENT will retrieve all recent batches since it'll meet one 'withdrawn'
- * FULL will retrieve all batches from current batch to 0-nth
- */
-enum Mode {
-  RECENT,
-  FULL,
-}
 
 const MODE: string = process.env.MODE;
 const CORE_CONTRACT: string = process.env.CORE_CONTRACT;
@@ -37,84 +29,125 @@ type BatchInfo = {
   unstaked_amount: string;
 };
 
+type CoreBatchInformaion = {
+  batch_id: number;
+  details: UnbondBatch;
+};
+
 /* Function print_n serves for getting information about 'n' first batches
  * current_unbond_batch - latest unbonding batch gotten from query
  * n - first n batches starting from current_unbond_batch
  * drop_client - drop client generated code from binary, used for queries
  */
-async function print_n(
-  current_unbond_batch: number,
-  n: number,
-  drop_client: DropCoreClient
-): Promise<Array<BatchInfo>> {
-  if (current_unbond_batch < n) {
-    return [];
-  }
-  const dropCoreConfig = await drop_client.queryConfig();
-  let arr: Array<BatchInfo> = [];
+async function create_batch_info(
+  dropCoreConfig: DropCoreConfig,
+  batch: CoreBatchInformaion
+): Promise<BatchInfo> {
+  const creation_date = new Date(batch.details.status_timestamps.new * 1000);
+  const creation_time = {
+    day: creation_date.getUTCDate().toString().padStart(2, "0"),
+    month: creation_date.getUTCMonth().toString().padStart(2, "0"),
+    year: creation_date.getUTCFullYear(),
+    hours: creation_date.getUTCHours().toString().padStart(2, "0"),
+    minutes: creation_date.getUTCMinutes().toString().padStart(2, "0"),
+    seconds: creation_date.getUTCSeconds().toString().padStart(2, "0"),
+  };
 
-  for (; n >= 0; n -= 1, current_unbond_batch -= 1) {
-    let batch: UnbondBatch = await drop_client.queryUnbondBatch({
-      batch_id: current_unbond_batch.toString(),
-    });
-
-    const creation_date = new Date(batch.status_timestamps.new * 1000);
-    const creation_time = {
-      day: creation_date.getUTCDate().toString().padStart(2, "0"),
-      month: creation_date.getUTCMonth().toString().padStart(2, "0"),
-      year: creation_date.getUTCFullYear(),
-      hours: creation_date.getUTCHours().toString().padStart(2, "0"),
-      minutes: creation_date.getUTCMinutes().toString().padStart(2, "0"),
-      seconds: creation_date.getUTCSeconds().toString().padStart(2, "0"),
+  let batch_details: BatchInfo = {
+    batch_id: batch.batch_id,
+    status: batch.details.status,
+    expected_amount: batch.details.expected_amount,
+    creation_time: `${creation_time.day}/${creation_time.month}/${creation_time.year}(${creation_time.hours}:${creation_time.minutes}:${creation_time.seconds})`,
+    expected_finalization_time: null,
+    unstaked_amount: batch.details.unbonded_amount,
+  };
+  if (batch.details.status !== "new") {
+    const expected_finalization_date = new Date(
+      1000 *
+        (batch.details.status_timestamps.unbond_requested +
+          dropCoreConfig.unbonding_period +
+          dropCoreConfig.unbond_batch_switch_time)
+    );
+    const expected_finalization_time = {
+      day: expected_finalization_date.getUTCDate().toString().padStart(2, "0"),
+      month: expected_finalization_date
+        .getUTCMonth()
+        .toString()
+        .padStart(2, "0"),
+      year: expected_finalization_date.getUTCFullYear(),
+      hours: expected_finalization_date
+        .getUTCHours()
+        .toString()
+        .padStart(2, "0"),
+      minutes: expected_finalization_date
+        .getUTCMinutes()
+        .toString()
+        .padStart(2, "0"),
+      seconds: expected_finalization_date
+        .getUTCSeconds()
+        .toString()
+        .padStart(2, "0"),
     };
-
-    let batch_details: BatchInfo = {
-      batch_id: current_unbond_batch,
-      status: batch.status,
-      expected_amount: batch.expected_amount,
-      creation_time: `${creation_time.day}/${creation_time.month}/${creation_time.year}(${creation_time.hours}:${creation_time.minutes}:${creation_time.seconds})`,
-      expected_finalization_time: null,
-      unstaked_amount: batch.unbonded_amount,
-    };
-    if (batch.status !== "new") {
-      const expected_finalization_date = new Date(
-        1000 *
-          (batch.status_timestamps.unbond_requested +
-            dropCoreConfig.unbonding_period +
-            dropCoreConfig.unbond_batch_switch_time)
-      );
-      const expected_finalization_time = {
-        day: expected_finalization_date
-          .getUTCDate()
-          .toString()
-          .padStart(2, "0"),
-        month: expected_finalization_date
-          .getUTCMonth()
-          .toString()
-          .padStart(2, "0"),
-        year: expected_finalization_date.getUTCFullYear(),
-        hours: expected_finalization_date
-          .getUTCHours()
-          .toString()
-          .padStart(2, "0"),
-        minutes: expected_finalization_date
-          .getUTCMinutes()
-          .toString()
-          .padStart(2, "0"),
-        seconds: expected_finalization_date
-          .getUTCSeconds()
-          .toString()
-          .padStart(2, "0"),
-      };
-      batch_details.expected_finalization_time = `${expected_finalization_time.day}/${expected_finalization_time.month}/${expected_finalization_time.year}(${expected_finalization_time.hours}:${expected_finalization_time.minutes}:${expected_finalization_time.seconds})`;
-    }
-    arr.push(batch_details);
+    batch_details.expected_finalization_time = `${expected_finalization_time.day}/${expected_finalization_time.month}/${expected_finalization_time.year}(${expected_finalization_time.hours}:${expected_finalization_time.minutes}:${expected_finalization_time.seconds})`;
   }
 
-  return arr;
+  return batch_details;
 }
 
-async function main(mode: Mode): Promise<void> {
+async function handle_recent(
+  drop_client: DropCoreClient
+): Promise<Array<BatchInfo>> {
+  const res: Array<BatchInfo> = [];
+  const config = await drop_client.queryConfig();
+  for (
+    let current_batch = Number(await drop_client.queryCurrentUnbondBatch());
+    current_batch >= 0;
+    current_batch -= 1
+  ) {
+    const batch = await drop_client.queryUnbondBatch({
+      batch_id: current_batch.toString(),
+    });
+    if (batch.status === "withdrawn") {
+      break;
+    }
+    res.push(
+      await create_batch_info(config, {
+        batch_id: current_batch,
+        details: batch,
+      })
+    );
+  }
+  return res;
+}
+
+async function handle_full(
+  drop_client: DropCoreClient
+): Promise<Array<BatchInfo>> {
+  const res: Array<BatchInfo> = [];
+  const config = await drop_client.queryConfig();
+  for (
+    let current_batch = Number(await drop_client.queryCurrentUnbondBatch());
+    current_batch >= 0;
+    current_batch -= 1
+  ) {
+    const batch = await drop_client.queryUnbondBatch({
+      batch_id: current_batch.toString(),
+    });
+    res.push(
+      await create_batch_info(config, {
+        batch_id: current_batch,
+        details: batch,
+      })
+    );
+  }
+  return res;
+}
+
+/* There're 2 possible modes for script
+ * RECENT will retrieve all recent batches since it'll meet one 'withdrawn'
+ * FULL will retrieve all batches from current batch to 0-nth
+ */
+async function main(mode: string): Promise<void> {
   const mainWallet: DirectSecp256k1HdWallet =
     await DirectSecp256k1HdWallet.fromMnemonic(WALLET_MNEMONIC, {
       prefix: "neutron",
@@ -130,58 +163,19 @@ async function main(mode: Mode): Promise<void> {
   let res: Array<BatchInfo> = [];
 
   switch (mode) {
-    case Mode.RECENT: {
-      let unbond_batch_height = Number(
-        await drop_client.queryCurrentUnbondBatch()
-      );
-      let current_unbond_batch = unbond_batch_height;
-      let batch: UnbondBatch = await drop_client.queryUnbondBatch({
-        batch_id: current_unbond_batch.toString(),
-      });
-      let n = 0;
-
-      /* Get amount of batches that haven't withdrawn yet
-       * Provide given n as n-1 since count there starts with 0
-       */
-      while (current_unbond_batch > 0 && batch.status !== "withdrawn") {
-        current_unbond_batch -= 1;
-        batch = await drop_client.queryUnbondBatch({
-          batch_id: current_unbond_batch.toString(),
-        });
-        n += 1;
-      }
-      res = await print_n(unbond_batch_height, n == 0 ? 0 : n - 1, drop_client);
+    case "RECENT": {
+      res = await handle_recent(drop_client);
       break;
     }
-    case Mode.FULL: {
-      let current_unbond_batch = Number(
-        await drop_client.queryCurrentUnbondBatch()
-      );
-      res = await print_n(
-        current_unbond_batch,
-        current_unbond_batch,
-        drop_client
-      );
+    case "FULL": {
+      res = await handle_full(drop_client);
       break;
+    }
+    default: {
+      throw new Error(`Invalid mode given: ${mode}`);
     }
   }
   console.log(JSON.stringify(res));
 }
 
-let mode: Mode;
-
-switch (MODE) {
-  case "RECENT": {
-    mode = Mode.RECENT;
-    break;
-  }
-  case "FULL": {
-    mode = Mode.FULL;
-    break;
-  }
-  default: {
-    throw new Error(`Unknown mode given: ${MODE}`);
-  }
-}
-
-main(mode);
+main(MODE);
