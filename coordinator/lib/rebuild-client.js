d0deb3141505d3234f383664b2a5abe70b12ff1e
Victor Faust <81014877+faust403@users.noreply.github.com>
2024-07-10 17:15:55 +0200

    Move away from rebase

--- coordinator/lib/rebuild-client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const contracts2ts_1 = require("@neutron-org/contracts2ts");
-(0, contracts2ts_1.generate)({
-    src: '../../drop-contracts',
-    out: './src/generated/contractLib',
-});
