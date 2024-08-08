a67b0cb8b6083b6adf084c3b7465dbd16c6582ed
Victor Faust <81014877+faust403@users.noreply.github.com>
2024-08-08 14:08:05 +0200

    Add execute_ibc_transfer test

--- contracts/staker/src/tests.rs
+++ contracts/staker/src/tests.rs
@@ -230,117 +230,174 @@ fn test_register_ica() {
 
 #[test]
 fn test_ibc_transfer() {
-    let mut deps = mock_dependencies(&[]);
-    CONFIG
-        .save(deps.as_mut().storage, &get_default_config())
+    let mut deps = mock_dependencies(&[cosmwasm_std::Coin {
+        denom: "base_denom".to_string(),
+        amount: Uint128::from(10000u64),
+    }]);
+    {
+        CONFIG
+            .save(deps.as_mut().storage, &get_default_config())
+            .unwrap();
+        TX_STATE
+            .save(
+                deps.as_mut().storage,
+                &drop_staking_base::state::staker::TxState {
+                    status: drop_staking_base::state::staker::TxStateStatus::InProgress,
+                    seq_id: Some(0u64),
+                    transaction: Some(drop_staking_base::state::staker::Transaction::Stake {
+                        amount: Uint128::from(0u64),
+                    }),
+                    reply_to: Some("neutron1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhufaa6".to_string()),
+                },
+            )
+            .unwrap();
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("nobody", &coins(0u128, "untrn")),
+            drop_staking_base::msg::staker::ExecuteMsg::IBCTransfer {},
+        )
+        .unwrap_err();
+        assert_eq!(
+            res,
+            crate::error::ContractError::InvalidState {
+                reason: "tx_state is not idle".to_string()
+            }
+        );
+    }
+    {
+        let mut invalid_contract_balance_deps = mock_dependencies(&[cosmwasm_std::Coin {
+            denom: "base_denom".to_string(),
+            amount: Uint128::from(0u64),
+        }]);
+        CONFIG
+            .save(
+                invalid_contract_balance_deps.as_mut().storage,
+                &get_default_config(),
+            )
+            .unwrap();
+        TX_STATE
+            .save(
+                invalid_contract_balance_deps.as_mut().storage,
+                &drop_staking_base::state::staker::TxState {
+                    status: drop_staking_base::state::staker::TxStateStatus::Idle,
+                    seq_id: Some(0u64),
+                    transaction: Some(drop_staking_base::state::staker::Transaction::Stake {
+                        amount: Uint128::from(0u64),
+                    }),
+                    reply_to: Some("neutron1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhufaa6".to_string()),
+                },
+            )
+            .unwrap();
+        let res = execute(
+            invalid_contract_balance_deps.as_mut(),
+            mock_env(),
+            mock_info("nobody", &coins(0u128, "untrn")),
+            drop_staking_base::msg::staker::ExecuteMsg::IBCTransfer {},
+        )
+        .unwrap_err();
+        assert_eq!(
+            res,
+            crate::error::ContractError::InvalidFunds {
+                reason: "amount is less than min_ibc_transfer".to_string()
+            }
+        );
+    }
+    {
+        NON_STAKED_BALANCE
+            .save(deps.as_mut().storage, &Uint128::from(0u64))
+            .unwrap();
+        CONFIG
+            .save(deps.as_mut().storage, &get_default_config())
+            .unwrap();
+        TX_STATE
+            .save(
+                deps.as_mut().storage,
+                &drop_staking_base::state::staker::TxState {
+                    status: drop_staking_base::state::staker::TxStateStatus::Idle,
+                    seq_id: Some(0u64),
+                    transaction: Some(drop_staking_base::state::staker::Transaction::Stake {
+                        amount: Uint128::from(0u64),
+                    }),
+                    reply_to: Some("neutron1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhufaa6".to_string()),
+                },
+            )
+            .unwrap();
+        ICA.set_address(
+            deps.as_mut().storage,
+            "ica_address".to_string(),
+            "port_id".to_string(),
+            "channel_id".to_string(),
+        )
         .unwrap();
-    TX_STATE
-        .save(deps.as_mut().storage, &TxState::default())
+        deps.querier.add_custom_query_response(|_| {
+            to_json_binary(&MinIbcFeeResponse {
+                min_fee: IbcFee {
+                    recv_fee: vec![],
+                    ack_fee: coins(100, "untrn"),
+                    timeout_fee: coins(200, "untrn"),
+                },
+            })
+            .unwrap()
+        });
+        let env = mock_env();
+        let res = execute(
+            deps.as_mut(),
+            env.clone(),
+            mock_info("nobody", &coins(0u128, "untrn")),
+            drop_staking_base::msg::staker::ExecuteMsg::IBCTransfer {},
+        )
         .unwrap();
-    let msg = drop_staking_base::msg::staker::ExecuteMsg::IBCTransfer {};
-    // no money on the contract
-    let res = execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info("nobody", &[Coin::new(600u128, "untrn")]),
-        msg.clone(),
-    );
-    assert_eq!(
-        res,
-        Err(crate::error::ContractError::InvalidFunds {
-            reason: "amount is less than min_ibc_transfer".to_string()
-        })
-    );
-
-    let mut deps = mock_dependencies(&[Coin::new(10001, "base_denom")]);
-    deps.querier.add_custom_query_response(|_| {
-        to_json_binary(&MinIbcFeeResponse {
-            min_fee: IbcFee {
-                recv_fee: vec![],
-                ack_fee: coins(100, "local_denom"),
-                timeout_fee: coins(200, "local_denom"),
-            },
-        })
-        .unwrap()
-    });
-    CONFIG
-        .save(deps.as_mut().storage, &get_default_config())
-        .unwrap();
-    TX_STATE
-        .save(deps.as_mut().storage, &TxState::default())
-        .unwrap();
-    NON_STAKED_BALANCE
-        .save(deps.as_mut().storage, &Uint128::zero())
-        .unwrap();
-    ICA.set_address(deps.as_mut().storage, "ica_address", "port", "channel")
-        .unwrap();
-    let res = execute(
-        deps.as_mut(),
-        mock_env(),
-        mock_info("nobody", &[Coin::new(600u128, "untrn")]),
-        msg.clone(),
-    )
-    .unwrap();
-    println!("{:?}", res);
-
-    // assert_eq!(
-    //     res,
-    //     Response::new()
-    //         .add_event(
-    //             Event::new("crates.io:drop-neutron-contracts__drop-staker-register-ica")
-    //                 .add_attributes(vec![
-    //                     ("action", "register_ica"),
-    //                     ("connection_id", "connection"),
-    //                     ("ica_id", "drop_STAKER")
-    //                 ])
-    //         )
-    //         .add_submessage(SubMsg::new(CosmosMsg::Custom(
-    //             NeutronMsg::register_interchain_account(
-    //                 "connection".to_string(),
-    //                 "drop_STAKER".to_string(),
-    //                 Some(vec![Coin::new(400u128, "untrn")]),
-    //             )
-    //         )))
-    // );
-    // // already asked for registration
-    // let res = execute(
-    //     deps.as_mut(),
-    //     mock_env(),
-    //     mock_info("nobody", &[Coin::new(1000u128, "untrn")]),
-    //     msg.clone(),
-    // );
-    // assert_eq!(
-    //     res,
-    //     Err(crate::error::ContractError::Std(
-    //         cosmwasm_std::StdError::generic_err("ICA registration is in progress right now")
-    //     ))
-    // );
-    // // reopen timeouted ICA
-    // ICA.set_timeout(deps.as_mut().storage).unwrap();
-    // let res = execute(
-    //     deps.as_mut(),
-    //     mock_env(),
-    //     mock_info("nobody", &[Coin::new(1000u128, "untrn")]),
-    //     msg.clone(),
-    // )
-    // .unwrap();
-    // assert_eq!(
-    //     res,
-    //     Response::new()
-    //         .add_event(
-    //             Event::new("crates.io:drop-neutron-contracts__drop-staker-register-ica")
-    //                 .add_attributes(vec![
-    //                     ("action", "register_ica"),
-    //                     ("connection_id", "connection"),
-    //                     ("ica_id", "drop_STAKER")
-    //                 ])
-    //         )
-    //         .add_submessage(SubMsg::new(CosmosMsg::Custom(
-    //             NeutronMsg::register_interchain_account(
-    //                 "connection".to_string(),
-    //                 "drop_STAKER".to_string(),
-    //                 Some(vec![Coin::new(400u128, "untrn")]),
-    //             )
-    //         )))
-    // );
+        assert_eq!(
+            res,
+            cosmwasm_std::Response::new()
+                .add_submessage(cosmwasm_std::SubMsg {
+                    id: 131072u64,
+                    msg: cosmwasm_std::CosmosMsg::Custom(NeutronMsg::IbcTransfer {
+                        source_port: "port_id".to_string(),
+                        source_channel: "transfer_channel_id".to_string(),
+                        token: cosmwasm_std::Coin {
+                            denom: "base_denom".to_string(),
+                            amount: Uint128::from(10000u64),
+                        },
+                        sender: "cosmos2contract".to_string(),
+                        receiver: "ica_address".to_string(),
+                        timeout_height: neutron_sdk::sudo::msg::RequestPacketTimeoutHeight {
+                            revision_height: None,
+                            revision_number: None
+                        },
+                        timeout_timestamp: env
+                            .block
+                            .time
+                            .plus_seconds(CONFIG.load(deps.as_mut().storage).unwrap().timeout)
+                            .nanos(),
+                        memo: "".to_string(),
+                        fee: IbcFee {
+                            recv_fee: vec![],
+                            ack_fee: vec![cosmwasm_std::Coin {
+                                denom: "untrn".to_string(),
+                                amount: Uint128::from(100u64),
+                            }],
+                            timeout_fee: vec![cosmwasm_std::Coin {
+                                denom: "untrn".to_string(),
+                                amount: Uint128::from(200u64),
+                            }]
+                        }
+                    }),
+                    gas_limit: None,
+                    reply_on: cosmwasm_std::ReplyOn::Success
+                })
+                .add_event(
+                    cosmwasm_std::Event::new(
+                        "crates.io:drop-neutron-contracts__drop-staker-ibc_transfer".to_string()
+                    )
+                    .add_attributes(vec![
+                        cosmwasm_std::attr("action".to_string(), "ibc_transfer".to_string()),
+                        cosmwasm_std::attr("connection_id".to_string(), "connection".to_string()),
+                        cosmwasm_std::attr("ica_id".to_string(), "drop_STAKER".to_string()),
+                        cosmwasm_std::attr("pending_amount".to_string(), "10000".to_string()),
+                    ])
+                )
+        );
+    }
 }
