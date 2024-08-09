97e2accae57e588ea5a42dbf2a67c920cc4c8991
Victor Faust <81014877+faust403@users.noreply.github.com>
2024-08-09 18:34:11 +0200

    Patch test_execute_setup_protocol

--- contracts/puppeteer/src/tests.rs
+++ contracts/puppeteer/src/tests.rs
@@ -302,6 +302,33 @@ fn test_execute_update_config() {
 }
 
 #[test]
+fn test_execute_setup_protocol_sender_is_not_allowed() {
+    let mut deps = mock_dependencies(&[]);
+    base_init(&mut deps.as_mut());
+    deps.querier.add_custom_query_response(|_| {
+        to_json_binary(&MinIbcFeeResponse {
+            min_fee: get_standard_fees(),
+        })
+        .unwrap()
+    });
+    let res = crate::contract::execute(
+        deps.as_mut(),
+        mock_env(),
+        mock_info("not_allowed_sender", &[]),
+        drop_staking_base::msg::puppeteer::ExecuteMsg::SetupProtocol {
+            delegate_grantee: "delegate_grantee".to_string(),
+            rewards_withdraw_address: "rewards_withdraw_address".to_string(),
+        },
+    );
+    assert_eq!(
+        res.unwrap_err(),
+        drop_puppeteer_base::error::ContractError::Std(StdError::generic_err(
+            "Sender is not allowed"
+        ))
+    );
+}
+
+#[test]
 fn test_execute_setup_protocol() {
     let mut deps = mock_dependencies(&[]);
     deps.querier.add_custom_query_response(|_| {
@@ -311,66 +338,57 @@ fn test_execute_setup_protocol() {
         .unwrap()
     });
     let pupeteer_base = base_init(&mut deps.as_mut());
-    let msg = drop_staking_base::msg::puppeteer::ExecuteMsg::SetupProtocol {
-        delegate_grantee: "delegate_grantee".to_string(),
-        rewards_withdraw_address: "rewards_withdraw_address".to_string(),
-    };
     let res = crate::contract::execute(
         deps.as_mut(),
         mock_env(),
-        mock_info("not_allowed_sender", &[]),
-        msg.clone(),
-    );
-    assert_eq!(
-        res.unwrap_err(),
-        drop_puppeteer_base::error::ContractError::Std(StdError::generic_err(
-            "Sender is not allowed"
-        ))
-    );
-    let env = mock_env();
-    let res = crate::contract::execute(deps.as_mut(), env, mock_info("allowed_sender", &[]), msg)
-        .unwrap();
+        mock_info("allowed_sender", &[]),
+        drop_staking_base::msg::puppeteer::ExecuteMsg::SetupProtocol {
+            delegate_grantee: "delegate_grantee".to_string(),
+            rewards_withdraw_address: "rewards_withdraw_address".to_string(),
+        },
+    )
+    .unwrap();
     let authz_msg = {
-        let msg = cosmos_sdk_proto::cosmos::authz::v1beta1::MsgGrant {
-            granter: "ica_address".to_string(),
-            grantee: "delegate_grantee".to_string(),
-            grant: Some(cosmos_sdk_proto::cosmos::authz::v1beta1::Grant {
-                expiration: Some(prost_types::Timestamp {
-                    seconds: mock_env()
-                        .block
-                        .time
-                        .plus_days(365 * 120 + 30)
-                        .seconds()
-                        .try_into()
-                        .unwrap(),
-                    nanos: 0,
-                }),
-                authorization: Some(cosmos_sdk_proto::Any {
-                    type_url: "/cosmos.authz.v1beta1.GenericAuthorization".to_string(),
-                    value: cosmos_sdk_proto::cosmos::authz::v1beta1::GenericAuthorization {
-                        msg: "/cosmos.staking.v1beta1.MsgDelegate".to_string(),
-                    }
-                    .encode_to_vec(),
-                }),
-            }),
-        };
-        let mut buf = Vec::with_capacity(msg.encoded_len());
-        msg.encode(&mut buf).unwrap();
         neutron_sdk::bindings::types::ProtobufAny {
             type_url: "/cosmos.authz.v1beta1.MsgGrant".to_string(),
-            value: Binary::from(buf),
+            value: Binary::from(
+                cosmos_sdk_proto::cosmos::authz::v1beta1::MsgGrant {
+                    granter: "ica_address".to_string(),
+                    grantee: "delegate_grantee".to_string(),
+                    grant: Some(cosmos_sdk_proto::cosmos::authz::v1beta1::Grant {
+                        expiration: Some(prost_types::Timestamp {
+                            seconds: mock_env()
+                                .block
+                                .time
+                                .plus_days(365 * 120 + 30)
+                                .seconds()
+                                .try_into()
+                                .unwrap(),
+                            nanos: 0,
+                        }),
+                        authorization: Some(cosmos_sdk_proto::Any {
+                            type_url: "/cosmos.authz.v1beta1.GenericAuthorization".to_string(),
+                            value: cosmos_sdk_proto::cosmos::authz::v1beta1::GenericAuthorization {
+                                msg: "/cosmos.staking.v1beta1.MsgDelegate".to_string(),
+                            }
+                            .encode_to_vec(),
+                        }),
+                    }),
+                }
+                .encode_to_vec(),
+            ),
         }
     };
     let distribution_msg = {
-        let msg = cosmos_sdk_proto::cosmos::distribution::v1beta1::MsgSetWithdrawAddress {
-            delegator_address: "ica_address".to_string(),
-            withdraw_address: "rewards_withdraw_address".to_string(),
-        };
-        let mut buf = Vec::with_capacity(msg.encoded_len());
-        msg.encode(&mut buf).unwrap();
         neutron_sdk::bindings::types::ProtobufAny {
             type_url: "/cosmos.distribution.v1beta1.MsgSetWithdrawAddress".to_string(),
-            value: Binary::from(buf),
+            value: Binary::from(
+                cosmos_sdk_proto::cosmos::distribution::v1beta1::MsgSetWithdrawAddress {
+                    delegator_address: "ica_address".to_string(),
+                    withdraw_address: "rewards_withdraw_address".to_string(),
+                }
+                .encode_to_vec(),
+            ),
         }
     };
     assert_eq!(
@@ -400,6 +418,52 @@ fn test_execute_setup_protocol() {
                 rewards_withdraw_address: "rewards_withdraw_address".to_string(),
             })
         }
+    );
+}
+
+#[test]
+fn test_execute_setup_protocol_not_idle() {
+    let mut deps = mock_dependencies(&[]);
+    deps.querier.add_custom_query_response(|_| {
+        to_json_binary(&MinIbcFeeResponse {
+            min_fee: get_standard_fees(),
+        })
+        .unwrap()
+    });
+    let pupeteer_base = base_init(&mut deps.as_mut());
+    pupeteer_base
+        .tx_state
+        .save(
+            deps.as_mut().storage,
+            &drop_puppeteer_base::state::TxState {
+                seq_id: None,
+                status: drop_puppeteer_base::state::TxStateStatus::InProgress,
+                reply_to: Some("".to_string()),
+                transaction: Some(drop_puppeteer_base::msg::Transaction::SetupProtocol {
+                    interchain_account_id: "ica_address".to_string(),
+                    delegate_grantee: "delegate_grantee".to_string(),
+                    rewards_withdraw_address: "rewards_withdraw_address".to_string(),
+                }),
+            },
+        )
+        .unwrap();
+    let res = crate::contract::execute(
+        deps.as_mut(),
+        mock_env(),
+        mock_info("allowed_sender", &[]),
+        drop_staking_base::msg::puppeteer::ExecuteMsg::SetupProtocol {
+            delegate_grantee: "delegate_grantee".to_string(),
+            rewards_withdraw_address: "rewards_withdraw_address".to_string(),
+        },
+    )
+    .unwrap_err();
+    assert_eq!(
+        res,
+        drop_puppeteer_base::error::ContractError::NeutronError(NeutronError::Std(
+            cosmwasm_std::StdError::generic_err(
+                "Transaction txState is not equal to expected: Idle".to_string()
+            )
+        ))
     );
 }
 
