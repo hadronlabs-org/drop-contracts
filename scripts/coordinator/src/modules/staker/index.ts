189897731672d3df1970d63a961f57d0a94a3d40
Albert Andrejev <albert.andrejev@gmail.com>
2024-08-13 12:05:36 +0300

    Add min staker balance support

--- scripts/coordinator/src/modules/staker/index.ts
+++ scripts/coordinator/src/modules/staker/index.ts
@@ -1,14 +1,17 @@
 import { ManagerModule } from '../../types/Module';
-import { DropStaker } from 'drop-ts-client';
+import { DropCore, DropStaker } from 'drop-ts-client';
 import { StakerConfig } from './types/config';
 import { Context } from '../../types/Context';
 import pino from 'pino';
 import JSONBig from 'json-bigint';
+import { Uint64 } from '@cosmjs/math';
 
 const StakerContractClient = DropStaker.Client;
+const CoreContractClient = DropCore.Client;
 
 export class StakerModule extends ManagerModule {
   contractClient?: InstanceType<typeof StakerContractClient>;
+  coreContractClient?: InstanceType<typeof CoreContractClient>;
   icaAddress?: string;
 
   constructor(
@@ -21,6 +24,11 @@ export class StakerModule extends ManagerModule {
     this.contractClient = new DropStaker.Client(
       this.context.neutronSigningClient,
       this.config.contractAddress,
+    );
+
+    this.coreContractClient = new DropCore.Client(
+      this.context.neutronSigningClient,
+      this.config.coreContractAddress,
     );
   }
 
@@ -41,19 +49,40 @@ export class StakerModule extends ManagerModule {
       }
     }
 
-    const res = await this.contractClient.iBCTransfer(
-      this.context.neutronWalletAddress,
-      1.5,
-      undefined,
-      [
-        {
-          amount: this.context.config.neutron.icaFee,
-          denom: 'untrn',
-        },
-      ],
+    const { base_denom: baseDenom } =
+      await this.coreContractClient.queryConfig();
+
+    const balance = await this.context.neutronQueryClient.bank.balance(
+      this.config.contractAddress,
+      baseDenom,
     );
 
-    this.log.info(`IBC transfer response: ${JSONBig.stringify(res)}`);
+    const balanceAmount = Uint64.fromString(balance.amount);
+
+    if (balanceAmount >= this.config.stakerMinBalance) {
+      this.log.info(
+        `Transferring ${balanceAmount}${baseDenom} coins to the Neutron wallet...`,
+      );
+      const res = await this.contractClient.iBCTransfer(
+        this.context.neutronWalletAddress,
+        1.5,
+        undefined,
+        [
+          {
+            amount: this.context.config.neutron.icaFee,
+            denom: 'untrn',
+          },
+        ],
+      );
+
+      this.log.info(`IBC transfer response: ${JSONBig.stringify(res)}`);
+
+      return;
+    }
+
+    this.log.info(
+      `Staker balance (${balanceAmount}) is less than the minimum required balance of ${this.config.stakerMinBalance}`,
+    );
   }
 
   prepareConfig(): StakerConfig {
@@ -61,6 +90,12 @@ export class StakerModule extends ManagerModule {
       contractAddress:
         process.env.STAKER_CONTRACT_ADDRESS ||
         this.context.factoryContractHandler.factoryState.staker_contract,
+      coreContractAddress:
+        process.env.CORE_CONTRACT_ADDRESS ||
+        this.context.factoryContractHandler.factoryState.core_contract,
+      stakerMinBalance: Uint64.fromString(
+        process.env.STAKER_MIN_BALANCE ?? '1000000',
+      ),
     };
 
     return this.config;
--- scripts/coordinator/src/modules/staker/types/config.ts
+++ scripts/coordinator/src/modules/staker/types/config.ts
@@ -2,4 +2,6 @@ import { Uint64 } from '@cosmjs/math';
 
 export type StakerConfig = {
   contractAddress: string;
+  coreContractAddress: string;
+  stakerMinBalance: Uint64;
 };
