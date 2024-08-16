167a9dbda72285d1d8eceafda257703d35320e91
Sergey Ratiashvili <serg.s.r@gmail.com>
2024-08-16 15:32:15 +0200

    chore: file name typo

--- packages/base/src/msg/mod.rs
+++ packages/base/src/msg/mod.rs
@@ -7,7 +7,7 @@ pub mod proposal_votes;
 pub mod provider_proposals;
 pub mod pump;
 pub mod puppeteer;
-pub mod redepmtion_rate_adapter;
+pub mod redemption_rate_adapter;
 pub mod reward_handler;
 pub mod rewards_manager;
 pub mod splitter;
