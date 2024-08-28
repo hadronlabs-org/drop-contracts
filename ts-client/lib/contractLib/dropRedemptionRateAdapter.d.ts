1185c655dd8937f4274492d1f5beb71595206a06
Murad Karammaev <muradkarammaev@gmail.com>
2024-08-28 20:27:47 +0300

    fix: broken ottersec tests

--- ts-client/lib/contractLib/dropRedemptionRateAdapter.d.ts
+++ ts-client/lib/contractLib/dropRedemptionRateAdapter.d.ts
@@ -2,6 +2,16 @@ import { CosmWasmClient, SigningCosmWasmClient, ExecuteResult, InstantiateResult
 import { StdFee } from "@cosmjs/amino";
 import { Coin } from "@cosmjs/amino";
 /**
+ * A human readable address.
+ *
+ * In Cosmos, this is typically bech32 encoded. But for multi-chain smart contracts no assumptions should be made other than being UTF-8 encoded and of reasonable length.
+ *
+ * This type represents a validated address. It can be created in the following ways 1. Use `Addr::unchecked(input)` 2. Use `let checked: Addr = deps.api.addr_validate(input)?` 3. Use `let checked: Addr = deps.api.addr_humanize(canonical_addr)?` 4. Deserialize from JSON. This must only be done from JSON that was validated before such as a contract's state. `Addr` must not be used in messages sent by the user because this would result in unvalidated instances.
+ *
+ * This type is immutable. If you really need to mutate it (Really? Are you sure?), create a mutable copy using `let mut mutable = Addr::to_string()` and operate on that `String` instance.
+ */
+export type Addr = string;
+/**
  * Expiration represents a point in time when some event happens. It can compare with a BlockInfo and will return is_expired() == true once the condition is hit (and for every block in the future)
  */
 export type Expiration = {
@@ -64,7 +74,7 @@ export interface DropRedemptionRateAdapterSchema {
     [k: string]: unknown;
 }
 export interface Config {
-    core_contract: string;
+    core_contract: Addr;
     denom: string;
 }
 /**
@@ -93,9 +103,9 @@ export interface RedemptionRateArgs {
     params?: Binary | null;
 }
 export interface UpdateConfigArgs {
-    new_config: Config1;
+    new_config: UpdateConfig;
 }
-export interface Config1 {
+export interface UpdateConfig {
     core_contract: string;
     denom: string;
 }
