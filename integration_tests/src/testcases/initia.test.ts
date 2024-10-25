8923020a4ea9d31cbde3015353965e1a2b0f17b5
Albert Andrejev <albert.andrejev@gmail.com>
2024-10-25 15:32:11 +0300

    add missed field to initia test

--- integration_tests/src/testcases/initia.test.ts
+++ integration_tests/src/testcases/initia.test.ts
@@ -1032,10 +1032,12 @@ describe('Core', () => {
               {
                 valoper_address: validatorAddress,
                 weight: 1,
+                on_top: '0',
               },
               {
                 valoper_address: secondValidatorAddress,
                 weight: 1,
+                on_top: '0',
               },
             ],
           },
