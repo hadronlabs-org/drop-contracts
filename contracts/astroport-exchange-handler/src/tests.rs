7cd234730d8ffa50be8682acca4e79f2f26c66ee
Albert Andrejev <albert.andrejev@gmail.com>
2025-01-17 13:51:10 +0200

    Change contract name in test

--- contracts/astroport-exchange-handler/src/tests.rs
+++ contracts/astroport-exchange-handler/src/tests.rs
@@ -797,7 +797,7 @@ fn test_migrate_wrong_contract() {
 
     let deps_mut = deps.as_mut();
 
-    cw2::set_contract_version(deps_mut.storage, "Test_contract", "0.0.1").unwrap();
+    cw2::set_contract_version(deps_mut.storage, "wrong_name_contract", "0.0.1").unwrap();
 
     let res = crate::contract::migrate(
         deps.as_mut(),
@@ -808,7 +808,7 @@ fn test_migrate_wrong_contract() {
     assert_eq!(
         res,
         drop_staking_base::error::astroport_exchange_handler::ContractError::MigrationError {
-            storage_contract_name: "Test_contract".to_string(),
+            storage_contract_name: "wrong_name_contract".to_string(),
             contract_name: CONTRACT_NAME.to_string()
         }
     )
