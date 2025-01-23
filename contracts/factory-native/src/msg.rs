943ac3a96d0c8d0faacc61a291ed525698cad22a
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2025-01-23 21:25:14 +0100

    Remove unused macros

--- contracts/factory-native/src/msg.rs
+++ contracts/factory-native/src/msg.rs
@@ -2,7 +2,6 @@ use crate::state::{CodeIds, RemoteOpts};
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{CosmosMsg, Decimal, Uint128};
 use cw_ownable::cw_ownable_execute;
-use drop_macros::pausable;
 use drop_staking_base::msg::token::DenomMetadata;
 use neutron_sdk::bindings::msg::NeutronMsg;
 
@@ -61,12 +60,13 @@ pub enum ValidatorSetMsg {
 }
 
 #[cw_ownable_execute]
-#[pausable]
 #[cw_serde]
 pub enum ExecuteMsg {
     UpdateConfig(Box<UpdateConfigMsg>),
     Proxy(ProxyMsg),
     AdminExecute { msgs: Vec<CosmosMsg<NeutronMsg>> },
+    Pause {},
+    Unpause {},
 }
 #[cw_serde]
 pub struct MigrateMsg {}
