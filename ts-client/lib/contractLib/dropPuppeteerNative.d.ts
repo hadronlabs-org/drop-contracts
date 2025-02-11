816fe28b48630ed9dba1a6147625ab86410e0e83
Albert Andrejev <albert.andrejev@gmail.com>
2025-02-11 12:33:18 +0200

    ts client update after merge

--- ts-client/lib/contractLib/dropPuppeteerNative.d.ts
+++ ts-client/lib/contractLib/dropPuppeteerNative.d.ts
@@ -1,10 +1,5 @@
-import {
-  CosmWasmClient,
-  SigningCosmWasmClient,
-  ExecuteResult,
-  InstantiateResult,
-} from '@cosmjs/cosmwasm-stargate';
-import { StdFee } from '@cosmjs/amino';
+import { CosmWasmClient, SigningCosmWasmClient, ExecuteResult, InstantiateResult } from "@cosmjs/cosmwasm-stargate";
+import { StdFee } from "@cosmjs/amino";
 /**
  * A human readable address.
  *
@@ -24,16 +19,13 @@ export type Binary = string;
 /**
  * Expiration represents a point in time when some event happens. It can compare with a BlockInfo and will return is_expired() == true once the condition is hit (and for every block in the future)
  */
-export type Expiration =
-  | {
-      at_height: number;
-    }
-  | {
-      at_time: Timestamp;
-    }
-  | {
-      never: {};
-    };
+export type Expiration = {
+    at_height: number;
+} | {
+    at_time: Timestamp;
+} | {
+    never: {};
+};
 /**
  * A point in time in nanosecond precision.
  *
@@ -58,77 +50,67 @@ export type Timestamp = Uint64;
  * let b = Uint64::from(70u32); assert_eq!(b.u64(), 70); ```
  */
 export type Uint64 = string;
-export type Transaction =
-  | {
-      undelegate: {
+export type Transaction = {
+    undelegate: {
         batch_id: number;
         denom: string;
         interchain_account_id: string;
         items: [string, Uint128][];
-      };
-    }
-  | {
-      redelegate: {
+    };
+} | {
+    redelegate: {
         amount: number;
         denom: string;
         interchain_account_id: string;
         validator_from: string;
         validator_to: string;
-      };
-    }
-  | {
-      withdraw_reward: {
+    };
+} | {
+    withdraw_reward: {
         interchain_account_id: string;
         validator: string;
-      };
-    }
-  | {
-      tokenize_share: {
+    };
+} | {
+    tokenize_share: {
         amount: number;
         denom: string;
         interchain_account_id: string;
         validator: string;
-      };
-    }
-  | {
-      redeem_shares: {
+    };
+} | {
+    redeem_shares: {
         items: RedeemShareItem[];
-      };
-    }
-  | {
-      claim_rewards_and_optionaly_transfer: {
+    };
+} | {
+    claim_rewards_and_optionaly_transfer: {
         denom: string;
         interchain_account_id: string;
         transfer?: TransferReadyBatchesMsg | null;
         validators: string[];
-      };
-    }
-  | {
-      i_b_c_transfer: {
+    };
+} | {
+    i_b_c_transfer: {
         amount: number;
         denom: string;
         real_amount: number;
         reason: IBCTransferReason;
         recipient: string;
-      };
-    }
-  | {
-      stake: {
+    };
+} | {
+    stake: {
         amount: Uint128;
-      };
-    }
-  | {
-      transfer: {
+    };
+} | {
+    transfer: {
         interchain_account_id: string;
         items: [string, Coin][];
-      };
-    }
-  | {
-      setup_protocol: {
+    };
+} | {
+    setup_protocol: {
         interchain_account_id: string;
         rewards_withdraw_address: string;
-      };
     };
+};
 /**
  * A thin wrapper around u128 that is using strings for JSON encoding/decoding, such that the full u128 range can be used for clients that convert JSON numbers to floats, like JavaScript and jq.
  *
@@ -143,227 +125,140 @@ export type Transaction =
  * let c = Uint128::from(70u32); assert_eq!(c.u128(), 70); ```
  */
 export type Uint128 = string;
-export type IBCTransferReason = 'l_s_m_share' | 'delegate';
+export type IBCTransferReason = "l_s_m_share" | "delegate";
 export type ArrayOfTransaction = Transaction[];
-export type QueryExtMsg =
-  | {
-      delegations: {};
-    }
-  | {
-      balances: {};
-    }
-  | {
-      unbonding_delegations: {};
-    };
+export type QueryExtMsg = {
+    delegations: {};
+} | {
+    balances: {};
+} | {
+    unbonding_delegations: {};
+};
 /**
  * Actions that can be taken to alter the contract's ownership
  */
-export type UpdateOwnershipArgs =
-  | {
-      transfer_ownership: {
+export type UpdateOwnershipArgs = {
+    transfer_ownership: {
         expiry?: Expiration | null;
         new_owner: string;
-      };
-    }
-  | 'accept_ownership'
-  | 'renounce_ownership';
+    };
+} | "accept_ownership" | "renounce_ownership";
 export interface DropPuppeteerNativeSchema {
-  responses: Config | Binary | OwnershipForString | ArrayOfTransaction;
-  query: ExtensionArgs;
-  execute:
-    | SetupProtocolArgs
-    | DelegateArgs
-    | UndelegateArgs
-    | ClaimRewardsAndOptionalyTransferArgs
-    | UpdateConfigArgs
-    | RegisterBalanceAndDelegatorDelegationsQueryArgs
-    | UpdateOwnershipArgs;
-  instantiate?: InstantiateMsg;
-  [k: string]: unknown;
+    responses: Config | Binary | OwnershipForString | ArrayOfTransaction;
+    query: ExtensionArgs;
+    execute: SetupProtocolArgs | DelegateArgs | UndelegateArgs | ClaimRewardsAndOptionalyTransferArgs | UpdateConfigArgs | RegisterBalanceAndDelegatorDelegationsQueryArgs | UpdateOwnershipArgs;
+    instantiate?: InstantiateMsg;
+    [k: string]: unknown;
 }
 export interface Config {
-  allowed_senders: Addr[];
-  distribution_module_contract: Addr;
-  native_bond_provider: Addr;
-  remote_denom: string;
+    allowed_senders: Addr[];
+    distribution_module_contract: Addr;
 }
 /**
  * The contract's ownership info
  */
 export interface OwnershipForString {
-  /**
-   * The contract's current owner. `None` if the ownership has been renounced.
-   */
-  owner?: string | null;
-  /**
-   * The deadline for the pending owner to accept the ownership. `None` if there isn't a pending ownership transfer, or if a transfer exists and it doesn't have a deadline.
-   */
-  pending_expiry?: Expiration | null;
-  /**
-   * The account who has been proposed to take over the ownership. `None` if there isn't a pending ownership transfer.
-   */
-  pending_owner?: string | null;
+    /**
+     * The contract's current owner. `None` if the ownership has been renounced.
+     */
+    owner?: string | null;
+    /**
+     * The deadline for the pending owner to accept the ownership. `None` if there isn't a pending ownership transfer, or if a transfer exists and it doesn't have a deadline.
+     */
+    pending_expiry?: Expiration | null;
+    /**
+     * The account who has been proposed to take over the ownership. `None` if there isn't a pending ownership transfer.
+     */
+    pending_owner?: string | null;
 }
 export interface RedeemShareItem {
-  amount: Uint128;
-  local_denom: string;
-  remote_denom: string;
+    amount: Uint128;
+    local_denom: string;
+    remote_denom: string;
 }
 export interface TransferReadyBatchesMsg {
-  amount: Uint128;
-  batch_ids: number[];
-  emergency: boolean;
-  recipient: string;
+    amount: Uint128;
+    batch_ids: number[];
+    emergency: boolean;
+    recipient: string;
 }
 export interface Coin {
-  amount: Uint128;
-  denom: string;
-  [k: string]: unknown;
+    amount: Uint128;
+    denom: string;
+    [k: string]: unknown;
 }
 export interface ExtensionArgs {
-  msg: QueryExtMsg;
+    msg: QueryExtMsg;
 }
 export interface SetupProtocolArgs {
-  rewards_withdraw_address: string;
+    rewards_withdraw_address: string;
 }
 export interface DelegateArgs {
-  items: [string, Uint128][];
-  reply_to: string;
+    items: [string, Uint128][];
+    reply_to: string;
 }
 export interface UndelegateArgs {
-  batch_id: number;
-  items: [string, Uint128][];
-  reply_to: string;
+    batch_id: number;
+    items: [string, Uint128][];
+    reply_to: string;
 }
 export interface ClaimRewardsAndOptionalyTransferArgs {
-  reply_to: string;
-  transfer?: TransferReadyBatchesMsg | null;
-  validators: string[];
+    reply_to: string;
+    transfer?: TransferReadyBatchesMsg | null;
+    validators: string[];
 }
 export interface UpdateConfigArgs {
-  new_config: ConfigOptional;
+    new_config: ConfigOptional;
 }
 export interface ConfigOptional {
-  allowed_senders?: string[] | null;
-  distribution_module_contract?: string | null;
-  native_bond_provider?: string | null;
-  remote_denom?: string | null;
+    allowed_senders?: string[] | null;
+    distribution_module_contract?: string | null;
 }
 export interface RegisterBalanceAndDelegatorDelegationsQueryArgs {
-  validators: string[];
+    validators: string[];
 }
 export interface InstantiateMsg {
-  allowed_senders: string[];
-  distribution_module_contract: string;
-  native_bond_provider: string;
-  owner?: string | null;
-  remote_denom: string;
+    allowed_senders: string[];
+    distribution_module_contract: string;
+    owner?: string | null;
 }
 export declare class Client {
-  private readonly client;
-  contractAddress: string;
-  constructor(
-    client: CosmWasmClient | SigningCosmWasmClient,
-    contractAddress: string,
-  );
-  mustBeSigningClient(): Error;
-  static instantiate(
-    client: SigningCosmWasmClient,
-    sender: string,
-    codeId: number,
-    initMsg: InstantiateMsg,
-    label: string,
-    fees: StdFee | 'auto' | number,
-    initCoins?: readonly Coin[],
-    admin?: string,
-  ): Promise<InstantiateResult>;
-  static instantiate2(
-    client: SigningCosmWasmClient,
-    sender: string,
-    codeId: number,
-    salt: Uint8Array,
-    initMsg: InstantiateMsg,
-    label: string,
-    fees: StdFee | 'auto' | number,
-    initCoins?: readonly Coin[],
-    admin?: string,
-  ): Promise<InstantiateResult>;
-  queryConfig: () => Promise<Config>;
-  queryTransactions: () => Promise<ArrayOfTransaction>;
-  queryExtension: (args: ExtensionArgs) => Promise<Binary>;
-  queryOwnership: () => Promise<OwnershipForString>;
-  setupProtocol: (
-    sender: string,
-    args: SetupProtocolArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  setupProtocolMsg: (args: SetupProtocolArgs) => {
-    setup_protocol: SetupProtocolArgs;
-  };
-  delegate: (
-    sender: string,
-    args: DelegateArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  delegateMsg: (args: DelegateArgs) => {
-    delegate: DelegateArgs;
-  };
-  undelegate: (
-    sender: string,
-    args: UndelegateArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  undelegateMsg: (args: UndelegateArgs) => {
-    undelegate: UndelegateArgs;
-  };
-  claimRewardsAndOptionalyTransfer: (
-    sender: string,
-    args: ClaimRewardsAndOptionalyTransferArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  claimRewardsAndOptionalyTransferMsg: (
-    args: ClaimRewardsAndOptionalyTransferArgs,
-  ) => {
-    claim_rewards_and_optionaly_transfer: ClaimRewardsAndOptionalyTransferArgs;
-  };
-  updateConfig: (
-    sender: string,
-    args: UpdateConfigArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  updateConfigMsg: (args: UpdateConfigArgs) => {
-    update_config: UpdateConfigArgs;
-  };
-  registerBalanceAndDelegatorDelegationsQuery: (
-    sender: string,
-    args: RegisterBalanceAndDelegatorDelegationsQueryArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  registerBalanceAndDelegatorDelegationsQueryMsg: (
-    args: RegisterBalanceAndDelegatorDelegationsQueryArgs,
-  ) => {
-    register_balance_and_delegator_delegations_query: RegisterBalanceAndDelegatorDelegationsQueryArgs;
-  };
-  updateOwnership: (
-    sender: string,
-    args: UpdateOwnershipArgs,
-    fee?: number | StdFee | 'auto',
-    memo?: string,
-    funds?: Coin[],
-  ) => Promise<ExecuteResult>;
-  updateOwnershipMsg: (args: UpdateOwnershipArgs) => {
-    update_ownership: UpdateOwnershipArgs;
-  };
+    private readonly client;
+    contractAddress: string;
+    constructor(client: CosmWasmClient | SigningCosmWasmClient, contractAddress: string);
+    mustBeSigningClient(): Error;
+    static instantiate(client: SigningCosmWasmClient, sender: string, codeId: number, initMsg: InstantiateMsg, label: string, fees: StdFee | 'auto' | number, initCoins?: readonly Coin[], admin?: string): Promise<InstantiateResult>;
+    static instantiate2(client: SigningCosmWasmClient, sender: string, codeId: number, salt: Uint8Array, initMsg: InstantiateMsg, label: string, fees: StdFee | 'auto' | number, initCoins?: readonly Coin[], admin?: string): Promise<InstantiateResult>;
+    queryConfig: () => Promise<Config>;
+    queryTransactions: () => Promise<ArrayOfTransaction>;
+    queryExtension: (args: ExtensionArgs) => Promise<Binary>;
+    queryOwnership: () => Promise<OwnershipForString>;
+    setupProtocol: (sender: string, args: SetupProtocolArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    setupProtocolMsg: (args: SetupProtocolArgs) => {
+        setup_protocol: SetupProtocolArgs;
+    };
+    delegate: (sender: string, args: DelegateArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    delegateMsg: (args: DelegateArgs) => {
+        delegate: DelegateArgs;
+    };
+    undelegate: (sender: string, args: UndelegateArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    undelegateMsg: (args: UndelegateArgs) => {
+        undelegate: UndelegateArgs;
+    };
+    claimRewardsAndOptionalyTransfer: (sender: string, args: ClaimRewardsAndOptionalyTransferArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    claimRewardsAndOptionalyTransferMsg: (args: ClaimRewardsAndOptionalyTransferArgs) => {
+        claim_rewards_and_optionaly_transfer: ClaimRewardsAndOptionalyTransferArgs;
+    };
+    updateConfig: (sender: string, args: UpdateConfigArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    updateConfigMsg: (args: UpdateConfigArgs) => {
+        update_config: UpdateConfigArgs;
+    };
+    registerBalanceAndDelegatorDelegationsQuery: (sender: string, args: RegisterBalanceAndDelegatorDelegationsQueryArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    registerBalanceAndDelegatorDelegationsQueryMsg: (args: RegisterBalanceAndDelegatorDelegationsQueryArgs) => {
+        register_balance_and_delegator_delegations_query: RegisterBalanceAndDelegatorDelegationsQueryArgs;
+    };
+    updateOwnership: (sender: string, args: UpdateOwnershipArgs, fee?: number | StdFee | "auto", memo?: string, funds?: Coin[]) => Promise<ExecuteResult>;
+    updateOwnershipMsg: (args: UpdateOwnershipArgs) => {
+        update_ownership: UpdateOwnershipArgs;
+    };
 }
