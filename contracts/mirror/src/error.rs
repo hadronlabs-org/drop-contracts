c9461df5cb2b2b7067c77fdcb2a7f6a594f09d6c
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2025-02-11 14:13:59 +0100

    Check contract name

--- contracts/mirror/src/error.rs
+++ contracts/mirror/src/error.rs
@@ -39,6 +39,12 @@ pub enum ContractError {
 
     #[error("IBC timeout out of range")]
     IbcTimeoutOutOfRange,
+
+    #[error("Can't migrate from {storage_contract_name} to {contract_name}")]
+    MigrationError {
+        storage_contract_name: String,
+        contract_name: String,
+    },
 }
 
 impl From<semver::Error> for ContractError {
