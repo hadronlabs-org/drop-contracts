1c611fc2596b4678eaba23c612bcf34b8d3a06b5
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2025-02-16 12:59:59 +0100

    Separate response and if-block

--- contracts/unbonding-mirror/src/contract.rs
+++ contracts/unbonding-mirror/src/contract.rs
@@ -36,18 +36,14 @@ pub fn instantiate(
     deps.api.addr_validate(&msg.core_contract)?;
     deps.api.addr_validate(&msg.withdrawal_manager)?;
     deps.api.addr_validate(&msg.withdrawal_voucher)?;
-    if deps
+    let channel_response: cosmwasm_std::ChannelResponse = deps
         .querier
-        .query::<cosmwasm_std::ChannelResponse>(&cosmwasm_std::QueryRequest::Ibc(
-            IbcQuery::Channel {
-                channel_id: msg.source_channel.clone(),
-                port_id: Some(msg.source_port.clone()),
-            },
-        ))
-        .unwrap()
-        .channel
-        .is_none()
-    {
+        .query(&cosmwasm_std::QueryRequest::Ibc(IbcQuery::Channel {
+            channel_id: msg.source_channel.clone(),
+            port_id: Some(msg.source_port.clone()),
+        }))
+        .unwrap();
+    if channel_response.channel.is_none() {
         return Err(ContractError::SourceChannelNotFound);
     }
     CONFIG.save(
