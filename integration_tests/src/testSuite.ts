a661c6f469c3c0e2bb226d7256978a4ce2b20113
Murad <muradkarammaev@gmail.com>
2025-02-18 12:54:41 +0200

    Revert "chore: reduce log verbosity"
    This reverts commit 401d578132d1a20b6ae2389597bf2afb6d0a2320.

--- integration_tests/src/testSuite.ts
+++ integration_tests/src/testSuite.ts
@@ -15,7 +15,6 @@ import {
 } from '@neutron-org/cosmopark/lib/types';
 import { Suite } from 'vitest';
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
-
 const packageJSON = require(`${__dirname}/../package.json`);
 const VERSION = (process.env.CI ? '_' : ':') + packageJSON.version;
 const ORG = process.env.CI ? 'neutronorg/lionco-contracts:' : '';
@@ -189,7 +188,7 @@ const networkConfigs = {
     denom: 'untrn',
     image: `${ORG}neutron-test${VERSION}`,
     prefix: 'neutron',
-    loglevel: 'info',
+    loglevel: 'debug',
     trace: true,
     public: true,
     type: 'ics',
