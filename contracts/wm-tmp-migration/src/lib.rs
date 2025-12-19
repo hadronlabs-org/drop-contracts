a5f43aba20f289f85f3ae67ec4a91767544151ad
Sergey Ratiashvili <serg.s.r@gmail.com>
2025-12-19 20:30:21 +0100

    migration: wm nft transfers

---
+++ contracts/wm-tmp-migration/src/lib.rs
@@ -0,0 +1,70 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use drop_staking_base::{
+    error::core::ContractResult,
+    msg::{
+        core::{ExecuteMsg, InstantiateMsg, QueryMsg},
+        withdrawal_voucher::ExecuteMsg as VoucherExecuteMsg,
+    },
+};
+use neutron_sdk::bindings::{msg::NeutronMsg, query::NeutronQuery};
+
+#[cfg_attr(not(feature = "library"), cosmwasm_std::entry_point)]
+pub fn instantiate(
+    _deps: DepsMut<NeutronQuery>,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> ContractResult<Response<NeutronMsg>> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), cosmwasm_std::entry_point)]
+pub fn query(_deps: Deps<NeutronQuery>, _env: Env, _msg: QueryMsg) -> ContractResult<Binary> {
+    unimplemented!();
+}
+
+#[cfg_attr(not(feature = "library"), cosmwasm_std::entry_point)]
+pub fn execute(
+    _deps: DepsMut<NeutronQuery>,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> ContractResult<Response<NeutronMsg>> {
+    unimplemented!();
+}
+
+#[cw_serde]
+pub struct TransferMsg {
+    pub recipient: String,
+    pub token_id: String,
+}
+
+#[cw_serde]
+pub struct MigrateMsg {
+    pub nfts_transfers: Vec<TransferMsg>,
+}
+
+#[cfg_attr(not(feature = "library"), cosmwasm_std::entry_point)]
+pub fn migrate(
+    _deps: DepsMut<NeutronQuery>,
+    _env: Env,
+    msg: MigrateMsg,
+) -> ContractResult<Response<NeutronMsg>> {
+    let mut msgs = vec![];
+    for transfer in msg.nfts_transfers.iter() {
+        let transfer_msg = VoucherExecuteMsg::TransferNft {
+            recipient: transfer.recipient.clone(),
+            token_id: transfer.token_id.clone(),
+        };
+        let exec_msg = cosmwasm_std::CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+            contract_addr: "neutron1f4c2l9u8yekp99mexyhf7wxgrq3gjtuzgrqe90xaq75v7quulptqzef6up"
+                .to_string(),
+            msg: cosmwasm_std::to_json_binary(&transfer_msg)?,
+            funds: vec![],
+        });
+        msgs.push(exec_msg);
+    }
+
+    Ok(Response::new().add_messages(msgs))
+}
